b146b666c78cafa6a65f703982700e25198fdeb7
edielin <laclin127@gmail.com>
2018-08-16 14:20:40 +0800

    feat: landing page design v-1.0 (#9)

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -35,19 +35,21 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>Getting Started (or other categories)</a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>Guides (or other categories)</a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>API Reference (or other categories)</a>
+            <a href={this.docUrl('edge/intro', this.props.language)}>Getting Started</a>
+            <a href={this.docUrl('ui/intro', this.props.language)}>Guides</a>
+            <a href={this.docUrl('.', this.props.language)}>API Reference</a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>User Showcase</a>
-            <a href="http://stackoverflow.com/questions/tagged/" target="_blank" rel="noreferrer noopener">
-              Stack Overflow
+            {/* <a href={this.pageUrl('users.html', this.props.language)}>User Showcase</a> */}
+            <a href="https://www.facebook.com/MoxaInc" target="_blank" rel="noreferrer noopener">
+              Facebook
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a href="https://twitter.com/" target="_blank" rel="noreferrer noopener">
-              Twitter
+            <a href="https://www.youtube.com/user/moxavideo" target="_blank" rel="noreferrer noopener">
+              Youtube
+            </a>
+            <a href="https://www.linkedin.com/company/moxa/" target="_blank" rel="noreferrer noopener">
+              Linkedin
             </a>
           </div>
           <div>
@@ -68,7 +70,7 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
+        {/* <a
           href="https://code.facebook.com/projects/"
           target="_blank"
           rel="noreferrer noopener"
@@ -80,7 +82,7 @@ class Footer extends React.Component {
             width="170"
             height="45"
           />
-        </a>
+        </a> */}
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
